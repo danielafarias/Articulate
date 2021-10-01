c7ce0dcf450a29e156483053e41e4d12c53bc34a
Daniela Farias <farias.danicris@gmail.com>
2021-09-30 22:58:56 -0300

    Request de Post Feed

--- api/Api.js
+++ api/Api.js
@@ -16,8 +16,37 @@ export const login = async(username, password) => {
     });
 }
 
-export const post = async(content) => {
-    return await axios.post(baseUrl + '/feed', {
-        content
+// export const post = async(content) => {
+//     return await axios.post(baseUrl + '/feed', {
+//         content
+//     });
+// }
+
+export const post = async (
+    content
+  ) => {
+    return await axios({
+      method: "post",
+      url: baseUrl + '/feed',
+      data: {
+          content
+      },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
     });
-}
+  };
+
+  export const feed = async () => {
+    const response = await axios({
+      method: "get",
+      url: baseUrl,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "menu@beeveggie.com",
+      },
+    });
+  
+    return response.data;
+  };
